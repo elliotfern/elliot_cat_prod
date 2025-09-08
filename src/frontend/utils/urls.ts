b3a6b5ac509e0bb0e606f42adee72e9729b35885
Elliot Fernandez <elliot@hispantic.com>
2025-09-08 12:30:49 +0100

    Nova seccio intranet curriculum

--- src/frontend/utils/urls.ts
+++ src/frontend/utils/urls.ts
@@ -1,3 +1,3 @@
 export const DOMAIN_WEB = `https://elliot.cat` as const;
 export const DOMAIN_IMG = `https://media.elliot.cat` as const;
-export const API_BASE = `https://${DOMAIN_WEB}/api` as const;
+export const API_BASE = `${DOMAIN_WEB}/api` as const;
