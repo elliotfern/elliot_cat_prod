03796df0a5da3f35af13825cdafc1919c7e664a3
Elliot Fernandez <elliot@hispantic.com>
2025-10-02 14:17:34 +0100

    Noves pagines categoria enllaços

--- src/frontend/pages/adreces/taulaLlistatTemaId.ts
+++ src/frontend/pages/adreces/taulaLlistatTemaId.ts
@@ -1,37 +1,25 @@
 import { renderDynamicTable } from '../../components/renderTaula/taulaRender';
-import { getPageType } from '../../utils/urlPath';
 import { getIsAdmin } from '../../services/auth/isAdmin';
 import { TaulaDinamica } from '../../types/TaulaDinamica';
-import { SubTema } from '../../types/SubTema';
-import { Link } from '../../types/Link';
-import { formatData } from '../../utils/formataData';
 import { DOMAIN_WEB } from '../../utils/urls';
+import { Tema } from '../../types/Tema';
 
 export async function taulaLlistatTemaId(id: string) {
   const isAdmin = await getIsAdmin();
 
-  const columns: TaulaDinamica<Link>[] = [
-    {
-      header: 'Enllaç',
-      field: 'nom',
-      render: (_: unknown, row: Link) => `<a id="${row.id}" href="${row.web}" target="_blank">${row.nom}</a>`,
-    },
-    { header: 'Categoría', field: 'tema_ca' },
-
-    { header: 'Tema', field: 'sub_tema_ca' },
-
-    { header: 'Tipus', field: 'tipus_ca' },
-
-    { header: 'Última actualització', field: 'dateModified', render: (_: unknown, row: Link) => formatData(row.dateModified) },
-  ];
-
-  if (isAdmin) {
-    columns.push({
-      header: 'Accions',
-      field: 'id',
-      render: (_: unknown, row: Link) => `<a id="${row.id}" href="${DOMAIN_WEB}/gestio/adreces/modifica-link/${row.id}"><button type="button" class="button btn-petit">Modifica</button></a>`,
-    });
-  }
+  const columns: TaulaDinamica<Tema>[] = [
+   {
+         header: 'Tema',
+         field: 'tema_ca',
+         render: (_: unknown, row: Tema) => `<a id="${row.id}" href="${DOMAIN_WEB}/gestio/adreces/llistat-temes/${row.id}">${row.tema_ca}</a>`,
+       },
+     ];
+   
+     columns.push({
+       header: 'Accions',
+       field: 'id',
+       render: (_: unknown, row: Tema) => `<a id="${row.id}" title="Modifica" href="${DOMAIN_WEB}/gestio/adreces/modifica-tema/${row.id}"><button type="button" class="button btn-petit">Modifica</button></a>`,
+     });
 
   renderDynamicTable({
     url: `https://${window.location.host}/api/adreces/get/llistatLinksTemaId?id=${id}`,
