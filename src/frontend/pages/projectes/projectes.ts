e72d3dbc89a316b6e9372fc76ee909c711724c78
Elliot Fernandez <elliot@hispantic.com>
2026-02-12 11:13:11 +0100

    Nou formulari creació projecte i modificació, nous endpoints get, post i put i typescript per gestionar operacions

--- src/frontend/pages/projectes/projectes.ts
+++ src/frontend/pages/projectes/projectes.ts
@@ -7,13 +7,25 @@ const pageType = getPageType(url);
 
 export function projectes() {
   const id = parseInt(pageType[3], 10);
-  const idLocale = parseInt(pageType[4], 10);
 
-  if (pageType[1] === 'projectes') {
+  if (pageType[1] !== 'projectes') return;
+
+  if (pageType.length === 2) {
+    // /projectes
     void initProjectesHome();
-  } else if (pageType[2] === 'modifica-projecte') {
-    formProjecte(true, id);
-  } else if (pageType[2] === 'crea-projecte') {
-    formProjecte(false);
+    return;
+  }
+
+  switch (pageType[2]) {
+    case 'modifica-projecte':
+      formProjecte(true, id);
+      break;
+
+    case 'crea-projecte':
+      formProjecte(false);
+      break;
+
+    default:
+      void initProjectesHome();
   }
 }
