ad2bb42d8d56101c6d34498b9bee391b7b6fb73b
Elliot Fernandez <elliot@hispantic.com>
2026-02-12 13:19:14 +0100

    Retoc problema amb versio del dump al fer deploy

--- public/includes/footer-end.php
+++ public/includes/footer-end.php
@@ -1,4 +1,13 @@
-<script type="module" src="/dist/bundle.js?v=1.3.57"></script>
+<?php
+$verFile = dirname(__DIR__) . '/version.txt'; // public/includes -> public/version.txt
+$ver = 'dev';
+
+if (is_file($verFile)) {
+    $ver = trim((string) file_get_contents($verFile));
+    if ($ver === '') $ver = 'dev';
+}
+?>
+<script type="module" src="/dist/bundle.js?v=<?= htmlspecialchars($ver, ENT_QUOTES) ?>"></script>
 </body>
 
 </html>
