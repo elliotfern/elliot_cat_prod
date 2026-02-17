950cd5f30594603255966764cad1d71e88b88a95
Elliot Fernandez <elliot@hispantic.com>
2026-02-17 13:02:40 +0100

    Historia oberta - nou endpoint per llistat articles i vista typescript

--- src/backend/routes/area-privada-administradors.php
+++ src/backend/routes/area-privada-administradors.php
@@ -125,7 +125,7 @@ $routes = [
     APP_GESTIO . $url['historia'] . '/llistat-articles' => route(APP_INTRANET_DIR . APP_HISTORIA_DIR . 'llistat-articles.php'),
     APP_GESTIO . $url['historia'] . '/llistat-cursos' => route(APP_INTRANET_DIR . APP_HISTORIA_DIR . 'llistat-cursos.php'),
 
-    APP_GESTIO . $url['historia'] . '/fitxa-curs' => route(APP_INTRANET_DIR . APP_HISTORIA_DIR . 'fitxa-curs.php'),
+    APP_GESTIO . $url['historia'] . '/fitxa-curs/{id}' => route(APP_INTRANET_DIR . APP_HISTORIA_DIR . 'fitxa-curs.php'),
 
     APP_GESTIO . $url['historia'] . '/llistat-organitzacions' => route(APP_INTRANET_DIR . APP_HISTORIA_DIR . 'llistat-organitzacions.php'),
     APP_GESTIO . $url['historia'] . '/llistat-esdeveniments' => route(APP_INTRANET_DIR . APP_HISTORIA_DIR . 'llistat-esdeveniments.php'),
