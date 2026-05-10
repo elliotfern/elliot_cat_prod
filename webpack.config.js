e29b1c344c67c7e66410294b232cc8575bb968cf
Elliot Fernandez <elliot@hispantic.com>
2026-05-10 11:20:33 +0200

    Eliminació paquets no necessaris a npm

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,52 @@ import { fileURLToPath } from 'url';
 // Obtener el directorio actual
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default {
-  entry: './src/frontend/main.ts', // Punt d'entrada principal
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
+export default (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/frontend/main.ts', // Punt d'entrada principal
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                sourceMap: false,
+              },
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  scripts: {
-    dev: 'webpack --mode development',
-    build: 'webpack --mode production',
-  },
-  devtool: 'source-map',
+    },
+
+    devtool: false,
+  };
 };
