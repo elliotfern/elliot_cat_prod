c0774f282d538b3361be1ab83eea03be89d72d6e
Elliot Fernandez <elliot@hispantic.com>
2026-05-13 10:55:38 +0200

    Correcció errors backend endpoints get

--- public/area-privada-administradors/11_cinema_series/vista-pelicula.php
+++ public/area-privada-administradors/11_cinema_series/vista-pelicula.php
@@ -2,7 +2,6 @@
 $slug = $routeParams[0];
 ?>
 
-
 <div id="barraNavegacioContenidor"></div>
 
 <div class="container">
@@ -20,52 +19,10 @@ $slug = $routeParams[0];
     <?php endif; ?>
   </div>
 
-  <div class='fixaDades'>
-    <div class='columna imatge'>
-      <img id="nameImg" src='' class='img-thumbnail img-fluid rounded mx-auto d-block' style='height:auto;width:auto;max-width:auto' alt='Cartell' title='Cartell'>
-    </div>
-
-    <div class="columna">
-
-      <div class="quadre-detalls">
-        <p><strong>Títol original: </strong><span id="pelicula"></span></p>
-        <p><strong>Títol a Espanya:</strong> <span id="pelicula_es"></span></p>
-        <p><strong>Director/a: </strong><a id="directorUrl" href=""><span id="nom"></span> <span id="cognoms"></span></a></p>
-        <p><strong>País:</strong> <span id="pais_cat"></span></p>
-        <p><strong>Idioma original: </strong><span id="idioma_ca"></span></p>
-        <p><strong>Any d'estrena: </strong><span id="any"></span></p>
-        <p><strong>Gènere: </strong><span id="genere_ca"></span></p>
-        <p><strong>Pel·lícula vista el: </strong><span id="dataVista"></span></p>
-        <p><strong>Fitxa creada: </strong><span id="dateCreated"></span></p>
-        <p><strong>Fitxa actualizada: </strong> <span id="dateModified"></span></p>
-      </div>
-    </div>
-
-  </div>
-  <hr>
-  <div class="container" style="padding:20px;background-color:#ececec;margin-top:25px;margin-bottom:25px">
-    <h4>Crítica de la pel·lícula</h4>
-    <span id="descripcio"></span>
-  </div>
+  <div class='fitxaPeli'></div>
 
   <hr>
 
-  <h4>Actors:</h4>
-  <button onclick="window.location.href='<?php echo APP_INTRANET . $url['cinema']; ?>/inserir-actor-pelicula/<?php echo $slug; ?>'" class="button btn-gran btn-secondari">Afegir actor a la pel·lícula</button>
-
-  <div class="table-responsive">
-    <table class="table table-striped" id="booksAuthor">
-      <thead class="table-primary">
-        <tr>
-          <th></th>
-          <th>Actor:</th>
-          <th>Personatge</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody></tbody>
-    </table>
-  </div>
+  <div class='taulaActors'></div>
 
 </div>
