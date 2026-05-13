be99df150475b7dfef6d93e07f024f353d4f5130
Elliot Fernandez <elliot@hispantic.com>
2026-05-13 10:51:49 +0200

    Correcció errors backend endpoints get

--- src/frontend/pages/historiaOberta/historiaOberta.ts
+++ src/frontend/pages/historiaOberta/historiaOberta.ts
@@ -1,7 +1,5 @@
 import { getPageType } from '../../utils/urlPath';
 import { transmissioDadesDB } from '../../utils/actualitzarDades';
-import { fitxaPersona } from '../persona/fitxaPersona';
-import { construirTaula } from '../../services/api/construirTaula';
 import { renderBlogArticleView } from '../blog/article';
 import { renderHistoriaObertaListPaged } from './llistatHistoriaObertaPaged';
 import { taulaLlistatCursosHistoria } from './llistatCursos';
@@ -54,38 +52,10 @@ export function historiaOberta() {
   }
 
   if (pageType[3] === 'modifica-article') {
-  } else if (pageType[2] === 'fitxa-persona') {
-    fitxaPersona('/api/persones/get/?persona=', pageType[3], 'historia-persona', function (data) {
-      construirTaula('taula1', '/api/historia/get/?carrecsPersona=', data.id, ['Càrrec', 'Organització', 'Anys', 'Accions'], function (fila, columna) {
-        if (columna.toLowerCase() === 'càrrec') {
-          // Manejar el caso del título
-          return fila['carrec'];
-        } else if (columna.toLowerCase() === 'organització') {
-          return '<a href="' + window.location.origin + '/gestio/historia/fitxa-organitzacio/' + fila['slug'] + '">' + fila['organitzacio'] + '</a>';
-        } else if (columna.toLowerCase() === 'anys') {
-          return `${fila['anys']} / ${fila['carrecFi']}`;
-        } else if (columna.toLowerCase() === 'accions') {
-          return `<button onclick="window.location.href='${window.location.origin}/gestio/historia/modifica-persona-carrec/${fila['id']}'" class="button btn-petit">Modificar</button>`;
-        } else {
-          // Manejar otros casos
-          return fila[columna.toLowerCase()];
-        }
-      });
-
-      construirTaula('taula2', '/api/historia/get/?esdevenimentsPersona=', data.id, ['Esdeveniment', 'Any', 'Accions'], function (fila, columna) {
-        if (columna.toLowerCase() === 'esdeveniment') {
-          // Manejar el caso del título
-          return '<a href="' + window.location.origin + '/gestio/historia/fitxa-esdeveniment/' + fila['slug'] + '">' + fila['esdeveniment'] + '</a>';
-        } else if (columna.toLowerCase() === 'anys') {
-          return `${fila['esdeDataIAny']}`;
-        } else if (columna.toLowerCase() === 'accions') {
-          return `<button onclick="window.location.href='${window.location.origin}/gestio/historia/modifica-esdeveniment-persona/${fila['idEP']}'" class="button btn-petit">Modificar</button>`;
-        } else {
-          // Manejar otros casos
-          return fila[columna.toLowerCase()];
-        }
-      });
-    });
+  
+     //'/api/historia/get/?carrecsPersona=', data.id, ['Càrrec', 'Organització', 'Anys', 'Accions'], function (fila, columna) {
+     // /api/historia/get/?esdevenimentsPersona=', data.id, ['Esdeveniment', 'Any', 'Accions'], function (fila, columna) {
+       
   } else if (pageType[2] === 'nou-esdeveniment') {
     const form = document.getElementById('formEsdeveniment');
     if (form) {
