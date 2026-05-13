5453dfb69794358b03935fc63d198eccb0aafe8e
Elliot Fernandez <elliot@hispantic.com>
2026-05-13 18:33:05 +0200

    Modificació formulari serie tv

--- public/area-privada-administradors/11_cinema_series/form-serie.php
+++ public/area-privada-administradors/11_cinema_series/form-serie.php
@@ -164,7 +164,7 @@ if ($modificaBtn === 1) {
 
 <script>
   function formUpdate(id) {
-    let urlAjax = "/api/cinema/get/serieIntranet?=id" + id;
+    let urlAjax = "/api/cinema/get/serieIntranet?id=" + id;
 
     fetch(urlAjax, {
         method: "GET",
