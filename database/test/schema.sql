f3f943565174de5c45df33c72027234ed9ef4d46
Elliot Fernandez <elliot@hispantic.com>
2026-06-03 12:45:05 +0200

    Nou deploy amb tests i integracio de bd

--- database/test/schema.sql
+++ database/test/schema.sql
@@ -1,15 +1,15 @@
-CREATE TABLE ciutat (
+CREATE TABLE db_geo_ciutats (
   id VARCHAR(36) PRIMARY KEY,
   nom VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE agenda_event (
-  id VARCHAR(36) PRIMARY KEY,
+CREATE TABLE db_agenda_esdeveniments (
+  id BINARY(16) PRIMARY KEY,
   titol VARCHAR(255) NOT NULL,
   descripcio TEXT NULL,
   tipus VARCHAR(50) NOT NULL,
   lloc VARCHAR(255) NULL,
-  ciutat_id VARCHAR(36) NOT NULL,
+  ciutat_id BINARY(16) NOT NULL,
   data_inici DATETIME NOT NULL,
   data_fi DATETIME NULL,
   tot_el_dia TINYINT(1) NOT NULL,
