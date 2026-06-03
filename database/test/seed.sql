87f23351059133a178e630c2aa54cc416fb3492b
Elliot Fernandez <elliot@hispantic.com>
2026-06-03 12:52:04 +0200

    Nou deploy amb tests i integracio de bd

--- database/test/seed.sql
+++ database/test/seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO db_geo_ciutats (id, nom)
-VALUES ('019e7223-646d-71d1-abee-2ed1613c1d0e', 'Barcelona');
+VALUES (UNHEX(REPLACE('019e7223-646d-71d1-abee-2ed1613c1d0e','-','')), 'Barcelona');
 
 INSERT INTO db_geo_ciutats (id, nom)
-VALUES ('019e8ca8-3513-720e-9822-aaa735a8d893', 'Madrid');
+VALUES (UNHEX(REPLACE('019e7223-646d-71d1-abee-2ed1563c1d0e','-','')), 'Madrid');
